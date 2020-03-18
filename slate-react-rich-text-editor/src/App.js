21e8bf2079431c46c1376f0e301574238daa2567
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-18 22:08:47 +0100

    Basic text

--- slate-react-rich-text-editor/src/App.js
+++ slate-react-rich-text-editor/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+// Import React dependencies.
+import React, { useEffect, useMemo, useState, Component } from "react";
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable />
+    </Slate>
+  )
 }
 
 export default App;
